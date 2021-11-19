f43cbc4cf61af712ca0ed45f4d50548c68a643a1
Elakkuvan Rajamani <elakkuvan.rajamani@valtech.com>
2021-11-19 17:41:03 +0100

    Initial Commit

---
+++ RewriteGenerator/RewriteGenerator.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace RewriteGenerator
+{
+    partial class RewriteGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnOpenExcel = new System.Windows.Forms.Button();
+            this.btnGenerateSitemap = new System.Windows.Forms.Button();
+            this.txtRuleName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(376, 36);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.ReadOnly = true;
+            this.txtFileName.Size = new System.Drawing.Size(219, 22);
+            this.txtFileName.TabIndex = 0;
+            this.txtFileName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnOpenExcel
+            // 
+            this.btnOpenExcel.Location = new System.Drawing.Point(200, 124);
+            this.btnOpenExcel.Name = "btnOpenExcel";
+            this.btnOpenExcel.Size = new System.Drawing.Size(196, 32);
+            this.btnOpenExcel.TabIndex = 1;
+            this.btnOpenExcel.Text = "Open Excel";
+            this.btnOpenExcel.UseVisualStyleBackColor = true;
+            this.btnOpenExcel.Click += new System.EventHandler(this.btnOpenExcel_Click);
+            // 
+            // btnGenerateSitemap
+            // 
+            this.btnGenerateSitemap.Location = new System.Drawing.Point(402, 124);
+            this.btnGenerateSitemap.Name = "btnGenerateSitemap";
+            this.btnGenerateSitemap.Size = new System.Drawing.Size(193, 32);
+            this.btnGenerateSitemap.TabIndex = 2;
+            this.btnGenerateSitemap.Text = "Generate Sitemap";
+            this.btnGenerateSitemap.UseVisualStyleBackColor = true;
+            this.btnGenerateSitemap.Click += new System.EventHandler(this.btnGenerateSitemap_Click);
+            // 
+            // txtRuleName
+            // 
+            this.txtRuleName.Location = new System.Drawing.Point(376, 76);
+            this.txtRuleName.Name = "txtRuleName";
+            this.txtRuleName.Size = new System.Drawing.Size(219, 22);
+            this.txtRuleName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rule Name PreFix";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Path";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(119, 178);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(589, 324);
+            this.txtResult.TabIndex = 7;
+            this.txtResult.Text = "";
+            // 
+            // RewriteGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 533);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRuleName);
+            this.Controls.Add(this.btnGenerateSitemap);
+            this.Controls.Add(this.btnOpenExcel);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "RewriteGenerator";
+            this.Text = "RewriteGenerator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnOpenExcel;
+        private System.Windows.Forms.Button btnGenerateSitemap;
+        private System.Windows.Forms.TextBox txtRuleName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtResult;
+    }
+}
+
---
+++ RewriteGenerator/RewriteGenerator.cs
@@ -0,0 +1,87 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RewriteGenerator
+{
+    public partial class RewriteGenerator : Form
+    {
+        public string FileName = string.Empty;
+
+        private const string redirect_format = @"<rule name=""{0}"" stopProcessing=""true"">{1}{2}</rule>";
+        private const string match_url = @"<match url=""^{0}""/>";
+        private const string action = @"<action type=""Redirect"" url=""{0}""/>";
+        public RewriteGenerator()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Read the Excel File
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpenExcel_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "C:\\";
+            openFileDialog1.Title = "Browe the Files";
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileName = openFileDialog1.FileName;
+                txtFileName.Text = FileName;
+            }
+        }
+
+        /// <summary>
+        /// Process the Excel File to Generate the Sitemap
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGenerateSitemap_Click(object sender, EventArgs e)
+        {
+            DataTableCollection tableCollection;
+            StringBuilder redirects = new StringBuilder();
+            using (var stream = File.Open(FileName, FileMode.Open, FileAccess.Read))
+            {
+                using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                    });
+                    tableCollection = result.Tables;
+                    foreach (DataTable table in tableCollection)
+                    {
+                        DataRowCollection allRows = table.Rows;
+                        int count = 1;
+                        foreach (DataRow row in allRows)
+                        {
+                            string originalUrl = string.Format(match_url, GetReletiveUrl(row[1].ToString()) + "$");
+                            string redirectUrl = string.Format(action, row[0].ToString());
+                            redirects.AppendFormat(redirect_format,
+                                txtRuleName.Text + "-" + count, originalUrl, redirectUrl);
+                            count++;
+                        }
+                    }
+                }
+                stream.Close();
+                txtResult.Text = redirects.ToString();
+            }
+        }
+
+        private string GetReletiveUrl(string fullUrl)
+        {
+            var url = new Uri(fullUrl);
+            return url.PathAndQuery.TrimStart('/');
+        }
+    }
+}
